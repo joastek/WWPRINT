3eb8ca9f9683e21ebd0d031c5c799372223c19c8
joastek <joastek@gmail.com>
2023-06-12 21:45:49 +0200

    MainSection added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,44 @@
 import Image from "next/image";
-import styles from "./page.module.css";
-
+import Image1 from "../../public/image 2.png";
+import Image2 from "../../public/image 3.png";
+import Image3 from "../../public/image 4.png";
+import "../styles/Mainpage.scss";
 export default function Home() {
-  return <>tetst</>;
+  return (
+    <>
+      <div className="mainpage_container">
+        <div className="mainpage_fontcontainer">
+          <div className="mainpage_Aboutfontcontainer">
+            O naklejkach
+            <br />
+            wiemy <a>wszystko.</a>
+          </div>
+          <div className="mainpage_Whatfontcontainer">
+            Oferujemy profesjonalne naklejki do ścian, okien i mebli, wykonane z
+            dbałością o detale i estetykę. Dzięki naszym unikalnym wzorom i
+            precyzji wykonania, wnętrza nabiorą stylowego i efektownego
+            charakteru.
+          </div>
+          <button className="mainpage_button">Skontaktuj się!</button>
+        </div>
+        <div className="mainpage_imageContainer">
+          <Image
+            src={Image1}
+            alt="Design jednej z wykonanych naklejek"
+            className="mainpage_image1"
+          />
+          <Image
+            src={Image2}
+            alt="Design jednej z wykonanych naklejek"
+            className="mainpage_image2"
+          />
+          <Image
+            src={Image3}
+            alt="Design jednej z wykonanych naklejek"
+            className="mainpage_image3"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
