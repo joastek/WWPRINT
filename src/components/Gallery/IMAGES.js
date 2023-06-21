d7b16e03a389d3cb1e64288057916eb403361339
joastek <joastek@gmail.com>
2023-06-21 12:44:42 +0200

    another testing images

--- src/components/Gallery/IMAGES.js
+++ src/components/Gallery/IMAGES.js
@@ -28,34 +28,50 @@ const IMAGES = {
       .default,
     image22: require("../../../../Naklejki_I_etykiety/gacek.png").default,
     image23: require("../../../../Naklejki_I_etykiety/go.at_ink 2.png").default,
-    image24: require("../../../../Naklejki_I_etykiety/go.at_ink 2.png").default,
+ 
     image25: require("../../../../Naklejki_I_etykiety/go.at_ink.png").default,
     image26: require("../../../../Naklejki_I_etykiety/j_u_l_l_y 2.png").default,
     image27: require("../../../../Naklejki_I_etykiety/j_u_l_l_y 3.png").default,
     image28: require("../../../../Naklejki_I_etykiety/j_u_l_l_y.png").default,
     image29: require("../../../../Naklejki_I_etykiety/jagoda_matula.png")
       .default,
-    image30: require("../../../../Naklejki_I_etykiety/kaczka.png").default,
-    image30: require("../../../../Naklejki_I_etykiety/katarzynku.png").default,
-    image30: require("../../../../Naklejki_I_etykiety/krowa.png").default,
-    image30: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_baranek.png")
+    image31: require("../../../../Naklejki_I_etykiety/kaczka.png").default,
+    image32: require("../../../../Naklejki_I_etykiety/katarzynku.png").default,
+    image33: require("../../../../Naklejki_I_etykiety/krowa.png").default,
+    image34: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_baranek.png")
       .default,
-    image30: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_dino.png")
+    image35: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_dino.png")
       .default,
-    image30: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_hot.png")
+    image36: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_hot.png")
       .default,
-    image30: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_koteł.png")
+    image37: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_koteł.png")
       .default,
-    image30: require("../../../../Naklejki_I_etykiety/mikrob_holo.png").default,
-    image30: require("../../../../Naklejki_I_etykiety/moje_slodkie_babush.png")
+    image38: require("../../../../Naklejki_I_etykiety/mikrob_holo.png").default,
+    image39: require("../../../../Naklejki_I_etykiety/moje_slodkie_babush.png")
       .default,
-    image30: require("../../../../Naklejki_I_etykiety").default,
-    image30: require("../../../../Naklejki_I_etykiety").default,
-    image30: require("../../../../Naklejki_I_etykiety").default,
-    image30: require("../../../../Naklejki_I_etykiety").default,
-    image30: require("../../../../Naklejki_I_etykiety").default,
-    image30: require("../../../../Naklejki_I_etykiety").default,
-    image30: require("../../../../Naklejki_I_etykiety").default,
+    image41: require("../../../../Naklejki_I_etykiety/moje_słodkie_babushka_tattooolsztyn.png").default,
+    image42: require("../../../../Naklejki_I_etykiety/nasta.png").default,
+    image43: require("../../../../Naklejki_I_etykiety/nasta_2.png").default,
+    image44: require("../../../../Naklejki_I_etykiety/noe6.png").default,
+    image45: require("../../../../Naklejki_I_etykiety/not_typical_tattoo_studio.png").default,
+    image46: require("../../../../Naklejki_I_etykiety/oli_dziara.png").default,
+    image47: require("../../../../Naklejki_I_etykiety/oli_dziara2.png").default,
+    image48: require("../../../../Naklejki_I_etykiety/oli_dziara3.png").default,
+    image49: require("../../../../Naklejki_I_etykiety/pan_zielnik_1.png").default,
+    image50: require("../../../../Naklejki_I_etykiety/panzielnik.png").default,
+    image51: require("../../../../Naklejki_I_etykiety/popcorn11.png").default,
+    image52: require("../../../../Naklejki_I_etykiety/precelek_1.png").default,
+    image53: require("../../../../Naklejki_I_etykiety/precelek_2.png").default,
+    image54: require("../../../../Naklejki_I_etykiety/psacjent.png").default,
+    image55: require("../../../../Naklejki_I_etykiety/psiekaska.png").default,
+    image56: require("../../../../Naklejki_I_etykiety/redlinska.png").default,
+    image57: require("../../../../Naklejki_I_etykiety/smutek.png").default,
+    image58: require("../../../../Naklejki_I_etykiety/syrenka_kod_qr.png").default,
+    image59: require("../../../../Naklejki_I_etykiety/thuggin_clothes.png").default,
+    image60: require("../../../../Naklejki_I_etykiety/unnamed.png").default,
+    image61: require("../../../../Naklejki_I_etykiety/walentynki.jpg").default,
+    image62: require("../../../../Naklejki_I_etykiety/świece.jpg").default,
+   
   },
 };
 
