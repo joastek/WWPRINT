e12bb21b3a01f6660d1ce3aaa247406ef5b0f6d9
joastek <joastek@gmail.com>
2023-06-21 12:24:41 +0200

    testing images, too large sizes, trying to compresss it

--- src/app/Offert/Naklejki_i_etykiety/page.tsx
+++ src/app/Offert/Naklejki_i_etykiety/page.tsx
@@ -1,13 +1,27 @@
 import GalleryNavBar from "@/components/NavBars/GalleryNavBar";
 import "../../../styles/pages/Offert/Naklejki_i_etykiety/Naklejki_i_etykiety.scss";
 import IMAGES from "../../../components/Gallery/IMAGES";
+import Image from "next/image";
+import test from "../../../../public/gallery_images/anitrochepiercing-2.png";
 const StickersAndLables = () => {
   return (
     <>
       <GalleryNavBar />
       <div className="Labels_container">
-        <img src={IMAGES.LABELS.image1.src} alt="" />
-        <img src={IMAGES.LABELS.image1.src} alt="" className="test" />
+        <Image
+          src={IMAGES.LABELS.image1.src}
+          alt=""
+          className="test"
+          width={400}
+          height={400}
+        />
+        <Image
+          src={IMAGES.LABELS.image2.src}
+          alt=""
+          className="test"
+          width={400}
+          height={400}
+        />
         {/* <img src={IMAGES.LABELS.image2.src} alt="" />
         <img src={IMAGES.LABELS.image2.src} alt="" />
         <img src={IMAGES.LABELS.image2.src} alt="" /> */}
--- src/components/Gallery/IMAGES.js
+++ src/components/Gallery/IMAGES.js
@@ -1,94 +1,61 @@
 const IMAGES = {
   LABELS: {
-    image1: require("../../../public/gallery_images/anitrochepiercing-2.png")
+    image1: require("../../../../Naklejki_I_etykiety/Bunia.png").default,
+    image2: require("../../../../Naklejki_I_etykiety/Angelika_Czopek-min.png")
       .default,
-    // image2:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/Angelika_Czopek.png")
-    //     .default,
-    // image3:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image4:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image5:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image6:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image7:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image8:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image9:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image10:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image11:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image12:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image13:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image14:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image15:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image16:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image17:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image18:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image19:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image20:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image21:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image22:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image23:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image24:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image25:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image26:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image27:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image28:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image29:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
-    // image30:
-    //   require("../../../public/gallery_images/Naklejki_I_etykiety/4_dziary_rabarbary.png")
-    //     .default,
+    image3: require("../../../../Naklejki_I_etykiety/Kris_dragon.png").default,
+    image4: require("../../../../Naklejki_I_etykiety/Madzia_INK.png").default,
+    image5: require("../../../../Naklejki_I_etykiety/Nawłość.png").default,
+    image6: require("../../../../Naklejki_I_etykiety/Pazy.png").default,
+    image7: require("../../../../Naklejki_I_etykiety/Pieseł.png").default,
+    image8: require("../../../../Naklejki_I_etykiety/Rysula.png").default,
+    image9: require("../../../../Naklejki_I_etykiety/Voice_of_ink.png").default,
+    image10:
+      require("../../../../Naklejki_I_etykiety/anitrochepiercing-min.png")
+        .default,
+    image11: require("../../../../Naklejki_I_etykiety/aparat.png").default,
+    image12: require("../../../../Naklejki_I_etykiety/april.png").default,
+    image13: require("../../../../Naklejki_I_etykiety/april_2.png").default,
+    image14: require("../../../../Naklejki_I_etykiety/arrpwttt.png").default,
+    image15: require("../../../../Naklejki_I_etykiety/avokado_2.png").default,
+    image16: require("../../../../Naklejki_I_etykiety/awokado.png").default,
+    image17: require("../../../../Naklejki_I_etykiety/babushka.png").default,
+    image18: require("../../../../Naklejki_I_etykiety/coś.png").default,
+    image19: require("../../../../Naklejki_I_etykiety/czypsy.png").default,
+    image20: require("../../../../Naklejki_I_etykiety/dr.nika+tattoo.png")
+      .default,
+    image21: require("../../../../Naklejki_I_etykiety/drop_art_tattoo.png")
+      .default,
+    image22: require("../../../../Naklejki_I_etykiety/gacek.png").default,
+    image23: require("../../../../Naklejki_I_etykiety/go.at_ink 2.png").default,
+    image24: require("../../../../Naklejki_I_etykiety/go.at_ink 2.png").default,
+    image25: require("../../../../Naklejki_I_etykiety/go.at_ink.png").default,
+    image26: require("../../../../Naklejki_I_etykiety/j_u_l_l_y 2.png").default,
+    image27: require("../../../../Naklejki_I_etykiety/j_u_l_l_y 3.png").default,
+    image28: require("../../../../Naklejki_I_etykiety/j_u_l_l_y.png").default,
+    image29: require("../../../../Naklejki_I_etykiety/jagoda_matula.png")
+      .default,
+    image30: require("../../../../Naklejki_I_etykiety/kaczka.png").default,
+    image30: require("../../../../Naklejki_I_etykiety/katarzynku.png").default,
+    image30: require("../../../../Naklejki_I_etykiety/krowa.png").default,
+    image30: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_baranek.png")
+      .default,
+    image30: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_dino.png")
+      .default,
+    image30: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_hot.png")
+      .default,
+    image30: require("../../../../Naklejki_I_etykiety/krzycz_tatoo_koteł.png")
+      .default,
+    image30: require("../../../../Naklejki_I_etykiety/mikrob_holo.png").default,
+    image30: require("../../../../Naklejki_I_etykiety/moje_slodkie_babush.png")
+      .default,
+    image30: require("../../../../Naklejki_I_etykiety").default,
+    image30: require("../../../../Naklejki_I_etykiety").default,
+    image30: require("../../../../Naklejki_I_etykiety").default,
+    image30: require("../../../../Naklejki_I_etykiety").default,
+    image30: require("../../../../Naklejki_I_etykiety").default,
+    image30: require("../../../../Naklejki_I_etykiety").default,
+    image30: require("../../../../Naklejki_I_etykiety").default,
   },
 };
 
